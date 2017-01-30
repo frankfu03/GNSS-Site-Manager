32cb04b01273f6c994e1ae9596a55b5174d9c64a
Brooke Smith <brooke.smith@ga.gov.au>
2017-01-30 12:17:52 +1100

    GEOD-307 - reformat files first

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,42 +1,43 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { APP_BASE_HREF } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { HttpModule } from '@angular/http';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ModalModule } from 'ng2-bootstrap/ng2-bootstrap';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {APP_BASE_HREF} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {HttpModule} from '@angular/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import {ModalModule} from 'ng2-bootstrap/ng2-bootstrap';
 
-import { AppComponent } from './app.component';
-import { routes } from './app.routes';
-import { AboutModule } from './about/about.module';
-import { SharedModule } from './shared/shared.module';
-import { HomeModule } from './home/home.module';
-import { SiteInfoModule } from './site-info/site-info.module';
-import { SelectSiteModule } from './select-site/select-site.module';
-import { AutoHeightDirective } from './shared/global/auto-height.directive';
+import {AppComponent} from './app.component';
+import {routes} from './app.routes';
+import {AboutModule} from './about/about.module';
+import {SharedModule} from './shared/shared.module';
+import {HomeModule} from './home/home.module';
+import {SiteInfoModule} from './site-info/site-info.module';
+import {SelectSiteModule} from './select-site/select-site.module';
+import {AutoHeightDirective} from './shared/global/auto-height.directive';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    HttpModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot(routes),
-    ModalModule,
-    AboutModule,
-    SiteInfoModule,
-    SelectSiteModule,
-    HomeModule,
-    SharedModule.forRoot()
-  ],
-  declarations: [
-    AppComponent,
-    AutoHeightDirective
-  ],
-  providers: [{
-    provide: APP_BASE_HREF,
-    useValue: '<%= APP_BASE %>'
-  }],
-  bootstrap: [ AppComponent ]
+    imports: [
+        BrowserModule,
+        HttpModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot(routes),
+        ModalModule,
+        AboutModule,
+        SiteInfoModule,
+        SelectSiteModule,
+        HomeModule,
+        SharedModule.forRoot()
+    ],
+    declarations: [
+        AppComponent,
+        AutoHeightDirective
+    ],
+    providers: [{
+        provide: APP_BASE_HREF,
+        useValue: '<%= APP_BASE %>'
+    }],
+    bootstrap: [AppComponent]
 })
 
-export class AppModule { }
+export class AppModule {
+}
--- src/client/app/app.routes.ts
+++ src/client/app/app.routes.ts
@@ -1,16 +1,16 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-import { SelectSiteRoutes } from './select-site/index';
-import { SiteInfoRoutes } from './site-info/index';
-import { AboutRoutes } from './about/index';
+import {SelectSiteRoutes} from './select-site/index';
+import {SiteInfoRoutes} from './site-info/index';
+import {AboutRoutes} from './about/index';
 
 export const routes: Routes = [
-  ...SiteInfoRoutes,
-  ...SelectSiteRoutes,
-  ...AboutRoutes,
-  {
-    // Default
-    path: '**',
-    redirectTo: '/'
-  }
+    ...SiteInfoRoutes,
+    ...SelectSiteRoutes,
+    ...AboutRoutes,
+    {
+        // Default
+        path: '**',
+        redirectTo: '/'
+    }
 ];
